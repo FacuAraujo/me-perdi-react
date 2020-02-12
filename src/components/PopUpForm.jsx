a3a515f1ca15cfb9dfda0d959c920910db5a1345
Facundo <facuaaraujo@gmail.com>
2020-02-12 18:57:49 -0300

    Added new div in popUpForm

--- src/components/PopUpForm.jsx
+++ src/components/PopUpForm.jsx
@@ -1,46 +1,51 @@
 import React from 'react'
 
+// import styles
+import './popUpForm.scss';
+
 function PopUpForm() {
     return (
-        <div className="popup-form">
-            <div className="content">
-                <div className="close-button">X</div>
-                <div className="title">Publicar</div>
-                <form action="">
-                    <input type="file" name="photos" id="photos" />
+        <div className="popup">
+            <div className="popup-form">
+                <div className="content">
+                    <div className="close-button">X</div>
+                    <div className="title">Publicar</div>
+                    <form action="">
+                        <input type="file" name="photos" id="photos" />
 
-                    <div className="form-row">
-                        <div className="form-col">
-                            <label htmlFor="name" className="form-label">Nombre</label>                        
-                            <input type="text" name="name" id="name"/>    
+                        <div className="form-row">
+                            <div className="form-col">
+                                <label htmlFor="name" className="form-label">Nombre</label>                        
+                                <input type="text" name="name" id="name"/>    
+                            </div>
+
+                            <div className="form-col">
+                                <label htmlFor="address" className="form-label">Dirección</label>                        
+                                <input type="text" name="address" id="address" />    
+                            </div>                        
                         </div>
 
-                        <div className="form-col">
-                            <label htmlFor="address" className="form-label">Dirección</label>                        
-                            <input type="text" name="address" id="address" />    
-                        </div>                        
-                    </div>
+                        <div className="form-row">
+                            <label htmlFor="date" className="form-label">Fecha</label>                     
+                            <input type="date" name="date" id="date" />                        
+                        </div>
 
-                    <div className="form-row">
-                        <label htmlFor="date" className="form-label">Fecha</label>                     
-                        <input type="date" name="date" id="date" />                        
-                    </div>
+                        <div className="form-row">
+                            <label htmlFor="email" className="form-label">Email</label>                     
+                            <input type="email" name="email" id="email" />                        
+                        </div>
 
-                    <div className="form-row">
-                        <label htmlFor="email" className="form-label">Email</label>                     
-                        <input type="email" name="email" id="email" />                        
-                    </div>
+                        <div className="form-row">
+                            <label htmlFor="phone" className="form-label">Teléfono</label>                     
+                            <input type="number" name="phone" id="phone" />                        
+                        </div>
 
-                    <div className="form-row">
-                        <label htmlFor="phone" className="form-label">Teléfono</label>                     
-                        <input type="number" name="phone" id="phone" />                        
-                    </div>
-
-                    <div className="form-row">
-                        <label htmlFor="desc" className="form-label">Descripción</label>                     
-                        <textarea name="desc" id="desc" rows="5"></textarea>                        
-                    </div>                                      
-                </form>
+                        <div className="form-row">
+                            <label htmlFor="desc" className="form-label">Descripción</label>                     
+                            <textarea name="desc" id="desc" rows="5"></textarea>                        
+                        </div>                                      
+                    </form>
+                </div>
             </div>
         </div>
     )
