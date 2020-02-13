d1fe7c2d55136d6a6cce52e5bc6338d891018538
Facundo <facuaaraujo@gmail.com>
2020-02-12 21:32:40 -0300

    Styles for form

--- src/components/PopUpForm.jsx
+++ src/components/PopUpForm.jsx
@@ -1,49 +1,102 @@
-import React from 'react'
+import React from 'react';
 
 // import styles
 import './popUpForm.scss';
 
 function PopUpForm() {
+
+    function onFocus(event) {
+        let input = event.target
+        input.parentElement.classList.add('is-focus')      
+    }
+
+    function onBlur(event) {
+        let input = event.target
+        
+        if(!input.value){
+            input.parentElement.classList.remove('is-focus')   
+        }           
+    }
+
     return (
         <div className="popup">
             <div className="popup-form">
                 <div className="content">
                     <div className="close-button">X</div>
                     <div className="title">Publicar</div>
-                    <form action="">
+                    <form action="" autoComplete="off">
                         <input type="file" name="photos" id="photos" />
 
-                        <div className="form-row">
-                            <div className="form-col">
-                                <label htmlFor="name" className="form-label">Nombre</label>                        
-                                <input type="text" name="name" id="name"/>    
+                        <div className="form-row form-row-col">
+                            <div className="form-col name-col">
+                                <label htmlFor="name" className="form-label">Nombre de tu mascota</label>                        
+                                <input 
+                                type="text" 
+                                name="name" 
+                                id="name"
+                                onFocus={(event) => onFocus(event)}
+                                onBlur={(event) => onBlur(event)}
+                                />    
                             </div>
 
-                            <div className="form-col">
-                                <label htmlFor="address" className="form-label">Dirección</label>                        
-                                <input type="text" name="address" id="address" />    
+                            <div className="form-col date-col">                                
+                                <label htmlFor="date" className="form-label">¿Cuándo?</label>                     
+                                <input 
+                                type="date" 
+                                name="date" 
+                                id="date" 
+                                />    
                             </div>                        
                         </div>
 
                         <div className="form-row">
-                            <label htmlFor="date" className="form-label">Fecha</label>                     
-                            <input type="date" name="date" id="date" />                        
+                            <label htmlFor="address" className="form-label">¿Donde?</label>                        
+                            <input 
+                            type="text" 
+                            name="address" 
+                            id="address"
+                            onFocus={(event) => onFocus(event)}
+                            onBlur={(event) => onBlur(event)}
+                            />                           
+                        </div>
+
+                        <div className="form-row form-row-col">
+                            <div className="form-col email-col">
+                                <label htmlFor="email" className="form-label">Tu Email</label>                     
+                                <input 
+                                type="email" 
+                                name="email" 
+                                id="email" 
+                                onFocus={(event) => onFocus(event)}
+                                onBlur={(event) => onBlur(event)} 
+                                />   
+                            </div>
+
+                            <div className="form-col phone-col">                              
+                                <label htmlFor="phone" className="form-label">Tu Teléfono</label>                     
+                                <input 
+                                type="number"
+                                name="phone" 
+                                id="phone" 
+                                onFocus={(event) => onFocus(event)}
+                                onBlur={(event) => onBlur(event)} 
+                                />    
+                            </div>                        
                         </div>
 
                         <div className="form-row">
-                            <label htmlFor="email" className="form-label">Email</label>                     
-                            <input type="email" name="email" id="email" />                        
+                            <label htmlFor="desc" className="form-label">Una descripción de tu mascota</label>                     
+                            <textarea 
+                            name="desc" 
+                            id="desc" 
+                            rows="5" 
+                            onFocus={(event) => onFocus(event)}
+                            onBlur={(event) => onBlur(event)}
+                            >
+                            </textarea>                        
                         </div>
 
-                        <div className="form-row">
-                            <label htmlFor="phone" className="form-label">Teléfono</label>                     
-                            <input type="number" name="phone" id="phone" />                        
-                        </div>
-
-                        <div className="form-row">
-                            <label htmlFor="desc" className="form-label">Descripción</label>                     
-                            <textarea name="desc" id="desc" rows="5"></textarea>                        
-                        </div>                                      
+                        <input type="submit" value="Publicar" className="btn" />                                    
                     </form>
                 </div>
             </div>
