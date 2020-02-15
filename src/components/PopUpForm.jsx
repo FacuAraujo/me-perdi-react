0ad88527663a6b228a9e643e2c72ff15909a8aa4
Facundo <facuaaraujo@gmail.com>
2020-02-15 01:15:14 -0300

    Button 'Publicar' hide/show functionality

--- src/components/PopUpForm.jsx
+++ src/components/PopUpForm.jsx
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 // import styles
 import './popUpForm.scss';
 
-function PopUpForm() {
+function PopUpForm(props) {
+
+    useEffect(() => {
+        document.addEventListener('keydown', props.handleKeydown)
+
+        return () => {document.removeEventListener('keydown', props.handleKeydown)} 
+    })
 
     function onFocus(event) {
         let input = event.target
@@ -19,87 +25,85 @@ function PopUpForm() {
     }
 
     return (
-        <div className="popup">
-            <div className="popup-form">
-                <div className="content">
-                    <div className="close-button">X</div>
-                    <div className="title">Publicar</div>
-                    <form action="" autoComplete="off">
-                        <input type="file" name="photos" id="photos" />
+        <div className="popup-form">            
+            <div className="content">
+                <button className="close-button" onClick={() => props.handleForm()}>X</button>
+                <div className="title">Publicar</div>
+                <form action="" autoComplete="off">
+                    <input type="file" name="photos" id="photos" />
 
-                        <div className="form-row form-row-col">
-                            <div className="form-col name-col">
-                                <label htmlFor="name" className="form-label">Nombre de tu mascota</label>                        
-                                <input 
-                                type="text" 
-                                name="name" 
-                                id="name"
-                                onFocus={(event) => onFocus(event)}
-                                onBlur={(event) => onBlur(event)}
-                                />    
-                            </div>
-
-                            <div className="form-col date-col">                                
-                                <label htmlFor="date" className="form-label">¿Cuándo?</label>                     
-                                <input 
-                                type="date" 
-                                name="date" 
-                                id="date" 
-                                />    
-                            </div>                        
-                        </div>
-
-                        <div className="form-row">
-                            <label htmlFor="address" className="form-label">¿Donde?</label>                        
+                    <div className="form-row form-row-col">
+                        <div className="form-col name-col">
+                            <label htmlFor="name" className="form-label">Nombre de tu mascota</label>                        
                             <input 
                             type="text" 
-                            name="address" 
-                            id="address"
+                            name="name" 
+                            id="name"
                             onFocus={(event) => onFocus(event)}
                             onBlur={(event) => onBlur(event)}
-                            />                           
+                            />    
                         </div>
 
-                        <div className="form-row form-row-col">
-                            <div className="form-col email-col">
-                                <label htmlFor="email" className="form-label">Tu Email</label>                     
-                                <input 
-                                type="email" 
-                                name="email" 
-                                id="email" 
-                                onFocus={(event) => onFocus(event)}
-                                onBlur={(event) => onBlur(event)} 
-                                />   
-                            </div>
+                        <div className="form-col date-col">                                
+                            <label htmlFor="date" className="form-label">¿Cuándo?</label>                     
+                            <input 
+                            type="date" 
+                            name="date" 
+                            id="date" 
+                            />    
+                        </div>                        
+                    </div>
 
-                            <div className="form-col phone-col">                              
-                                <label htmlFor="phone" className="form-label">Tu Teléfono</label>                     
-                                <input 
-                                type="number"
-                                name="phone" 
-                                id="phone" 
-                                onFocus={(event) => onFocus(event)}
-                                onBlur={(event) => onBlur(event)} 
-                                />    
-                            </div>                        
-                        </div>
+                    <div className="form-row">
+                        <label htmlFor="address" className="form-label">¿Donde?</label>                        
+                        <input 
+                        type="text" 
+                        name="address" 
+                        id="address"
+                        onFocus={(event) => onFocus(event)}
+                        onBlur={(event) => onBlur(event)}
+                        />                           
+                    </div>
 
-                        <div className="form-row">
-                            <label htmlFor="desc" className="form-label">Una descripción de tu mascota</label>                     
-                            <textarea 
-                            name="desc" 
-                            id="desc" 
-                            rows="5" 
+                    <div className="form-row form-row-col">
+                        <div className="form-col email-col">
+                            <label htmlFor="email" className="form-label">Tu Email</label>                     
+                            <input 
+                            type="email" 
+                            name="email" 
+                            id="email" 
                             onFocus={(event) => onFocus(event)}
-                            onBlur={(event) => onBlur(event)}
-                            >
-                            </textarea>                        
+                            onBlur={(event) => onBlur(event)} 
+                            />   
                         </div>
 
-                        <input type="submit" value="Publicar" className="btn" />                                    
-                    </form>
-                </div>
-            </div>
+                        <div className="form-col phone-col">                              
+                            <label htmlFor="phone" className="form-label">Tu Teléfono</label>                     
+                            <input 
+                            type="number"
+                            name="phone" 
+                            id="phone" 
+                            onFocus={(event) => onFocus(event)}
+                            onBlur={(event) => onBlur(event)} 
+                            />    
+                        </div>                        
+                    </div>
+
+                    <div className="form-row">
+                        <label htmlFor="desc" className="form-label">Una descripción de tu mascota</label>                     
+                        <textarea 
+                        name="desc" 
+                        id="desc" 
+                        rows="5" 
+                        onFocus={(event) => onFocus(event)}
+                        onBlur={(event) => onBlur(event)}
+                        >
+                        </textarea>                        
+                    </div>
+
+                    <input type="submit" value="Publicar" className="btn" />                                    
+                </form>
+            </div>            
         </div>
     )
 }
--- src/components/PublishButton.jsx
+++ src/components/PublishButton.jsx
@@ -1,18 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 // Import components
 import PopUpForm from './PopUpForm'
 // Import styles
 import './publishButton.scss'
 
-
 function PublishButton(){
-    return(
-        <div className="publish-button">
-            <button className="btn">Publicar</button>
-            <PopUpForm></PopUpForm>   
-        </div>
-    )
+    const [formVisible, setFormVisible] = useState(false)
+
+    function handleFormVisible(){
+        setFormVisible(false)
+    }
+
+    function handleEscapeOutside() {
+        setFormVisible(false)
+    }
+
+    function handleKeydown (event) {
+        if(event.key === 'Escape'){
+            setFormVisible(false)
+        }
+    }
+
+    if(formVisible){
+        return(
+            <div className="publish-button">
+                <button className="btn"  onClick={() => setFormVisible(true)}>Publicar</button>
+                <div className="popup">
+                    <PopUpForm 
+                    handleForm={handleFormVisible} 
+                    onEscapeOutside={handleEscapeOutside} 
+                    handleKeydown={handleKeydown}>
+                    </PopUpForm>
+                </div>
+            </div>
+        )   
+    }else {
+        return(
+            <div className="publish-button">
+                <button className="btn"  onClick={() => setFormVisible(true)}>Publicar</button>  
+            </div>
+        )
+    }
+
+    
 }
 
 export default PublishButton
